6a0f7800b0533248f5c10ca8b631a3988de5259a
vocongbinh <binh93phudong@gmail.com>
2024-11-01 18:16:14 +0700

    final

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -168,7 +168,7 @@ const CheckoutPage = () => {
               );
             }}
           </Formik>
-          <button onClick={handleOrder}>haha</button>
+          {/* <button onClick={handleOrder}>haha</button> */}
           <BottomBar>
             <MainButton text={`PAY $${total}`} onClick={handleOrder} />
           </BottomBar>
